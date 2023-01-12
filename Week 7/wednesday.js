96f9595a8968daac0db440d3b9137f7ad03c0d5a
KevinPalaciosQ <3001877180101@ingenieria.usac.edu.gt>
2023-01-11 19:34:10 -0600

    feat adding week 7

---
+++ Week 7/wednesday.js
@@ -0,0 +1,27 @@
+//Count strings in objects
+function strCount(obj){
+    let c = 0
+    for (key in obj){
+      if (typeof obj[key] == 'string') c++;
+      if (typeof obj[key] == 'object') c+= strCount(obj[key]);
+    }
+    
+    return c
+  }
+//Extending JavaScript Objects: Get First & Last Array Element
+Array.prototype.first = function() {
+    return this[0];
+  };
+  
+  Array.prototype.last = function() {
+    return this[this.length-1];
+  };
+//Object Oriented Piracy
+function Ship(d,c) { 
+    this.d = d
+    this.c = c;
+    
+    this.isWorthIt = function (){
+      return (this.d - this.c * 1.5) > 20;
+    }
+  }
